b60d6bb77aeb3048ccf830c6c20746f4531e581f
Vaishnavi Ganesh Mane <61860303+v29m01@users.noreply.github.com>
2024-11-29 17:53:24 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Portfolio website loaded successfully!');
 });
+function toggleMenu() {
+    var navList = document.getElementById('nav-list');
+    navList.classList.toggle('active');
+}
